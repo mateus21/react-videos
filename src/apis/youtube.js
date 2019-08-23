2257299e3a9afd13ad27175db0d9b1f91bff0d34
Mateus Mantovani <mateus21mantovani@hotmail.com>
2019-08-22 22:28:50 -0300

    Search video term

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,14 @@
-const KEY = 'AIz';
+import axios from 'axios';
+
+const KEY = 'AIzaSyAiDFZAM5GFqy1ZmzCNkBIDJ7ZnPDfIk34';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+    }
+});
+
+
