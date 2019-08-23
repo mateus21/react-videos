fe784dbd8f6426f39116eefaf9f04342445311f6
Mateus Mantovani <mateus21mantovani@hotmail.com>
2019-08-22 22:47:52 -0300

    Create VideoItem component

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -3,12 +3,10 @@ import axios from 'axios';
 const KEY = 'AIzaSyAiDFZAM5GFqy1ZmzCNkBIDJ7ZnPDfIk34';
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-        part: 'snippet',
-        maxResults: 5,
-        key: KEY,
-    }
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY,
+  }
 });
-
-
